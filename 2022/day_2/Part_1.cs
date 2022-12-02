3ca9b62f2870323f9151832850429cd4d9cfd523
Ivan Mickey Milosavljevic <ivan.m.milosavljevic@visma.com>
2022-12-03 00:02:38 +0100

    bump .net 7 & update day 2 part 1

--- 2022/day_2/Part_1.cs
+++ 2022/day_2/Part_1.cs
@@ -6,8 +6,7 @@ public partial class Day2_Part1
     {
         return (int)(me - opponent) switch
         {
-            -2 => 6,
-            1 => 6,
+            1 or -2 => 6,
             0 => 3,
             _ => 0 // 2, -1
         };
@@ -17,7 +16,7 @@ public partial class Day2_Part1
     {
         var result = Input.input.Split(Environment.NewLine);
         var score = result
-            .Select(s => new {Opponent = s[0], Me = (char)(s[2]-23)}) // convert to base X=>A Y=>B Z=>C
+            .Select(s => new {Opponent = s[0], Me = (char)(s[2]-23)})       // convert to base X=>A Y=>B Z=>C
             .Select(s => GetGamePoints(s.Opponent, s.Me) + (char)(s.Me-64)) // A-64 = 1; B-64 = 2; C-64 = 3
             .Sum();
             
